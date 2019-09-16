23331ed06bec5bbaf423554a0d6cace97e0b0b03
Karl Benedict <kbene@unm.edu>
2019-09-16 10:40:45 -0600

    changed over to use README.md instead of projectInfo.md

--- generate.sh
+++ generate.sh
@@ -1,1 +1,2 @@
-pandoc --standalone --css=base.css  -o index.html projectInfo.md 
+pandoc --standalone --css=base.css  -o index.html README.md
+pandoc --standalone --template=default.latex -o README.pdf README.md 
